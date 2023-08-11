2465aca9b2934ae5afa7733b7b98edd41a5d5ada
Warr <warr@1bit.com.ph>
2023-08-11 17:01:05 +0800

    updated

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -6,34 +6,39 @@ use App\Models\Client;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Spatie\PdfToImage\Pdf;
 
 class PublicController extends Controller
 {
     public function index(Request $request): View
     {
-        $client=$request->client;
-        return view('public',compact('client'));
+        $client = $request->client;
+        return view('public', compact('client'));
     }
-    public function uploadDoc(Request $request) {
+    public function uploadDoc(Request $request)
+    {
         $request->validate([
             'file' => 'required|mimes:docx,pdf'
 
         ]);
-        
+
         if ($request->hasFile('file')) {
             $file = $request->file('file');
             $filename = $file->getClientOriginalName();
-            $extension=pathinfo($file)['extension'];
-            $target_file = storage_path('app/thumbs/'. $request->client->getFolder() ). '/' . $filename . "." . $extension;
-            $pdf = new Pdf($f);
+            $extension = pathinfo($file)['extension'];
 
             // Move the uploaded file to a storage location
-            $file->storeAs('files/'.$request->client->getFilePath(), $filename);
+            $actual = $file->storeAs('files/' . $request->client->getFolder(), $filename);
+            $actual=storage_path('app/'.$actual);
 
-            $files=[];
+            $target_file = storage_path('app/thumbs/' . $request->client->getFolder()) . '/' . $filename . "." . $extension;
+            $pdf = new Pdf($actual);
+            $pdf->setPage(1)->saveImage($target_file);
+
+            $files = [];
 
             // return Response::success('document uploaded successful');
-            return view('partials.uploads',['files' => ['sda','sdas']]);
+            return view('partials.uploads', ['files' => ['sda', 'sdas']]);
         }
 
         return 'No file uploaded.';
