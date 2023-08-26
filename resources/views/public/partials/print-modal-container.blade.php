568e2f7d2028ed500607768b3ee68071b3fddf01
warrdev <warrdev08@gmail.com>
2023-08-27 03:22:02 +0800

    removed fixed loader

--- resources/views/public/partials/print-modal-container.blade.php
+++ resources/views/public/partials/print-modal-container.blade.php
@@ -1,8 +1,4 @@
  <!-- Modal -->
  <div aria-hidden="false" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="printme" class="modal fade">
-     <section class="sec-loading">
-         <div class="one">
-         </div>
-     </section>
  </div>
  <!-- modal -->
